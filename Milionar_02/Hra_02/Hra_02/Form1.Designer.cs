c98ab00f69eaaddbe183da1961e6ab5d2e8ea2d7
Mário Markovič <96201545+mario007101@users.noreply.github.com>
2023-03-23 21:46:04 +0100

    Add files via upload

---
+++ Milionar_02/Hra_02/Hra_02/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿
+namespace Hra_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.button1 = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.C = new System.Windows.Forms.Label();
+            this.D = new System.Windows.Forms.Label();
+            this.otazka = new System.Windows.Forms.Label();
+            this.dalsia_otazka = new System.Windows.Forms.Button();
+            this.z50 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hodnotenie = new System.Windows.Forms.Button();
+            this.zTelefon = new System.Windows.Forms.Label();
+            this.ukoncenie = new System.Windows.Forms.Button();
+            this.graf = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.zPublikum = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.priatel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.graf)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(1761, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A
+            // 
+            this.A.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A.Location = new System.Drawing.Point(181, 235);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(372, 113);
+            this.A.TabIndex = 1;
+            this.A.Text = "label1";
+            this.A.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // B
+            // 
+            this.B.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B.Location = new System.Drawing.Point(642, 235);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(372, 113);
+            this.B.TabIndex = 2;
+            this.B.Text = "label1";
+            this.B.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // C
+            // 
+            this.C.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C.Location = new System.Drawing.Point(181, 357);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(372, 113);
+            this.C.TabIndex = 3;
+            this.C.Text = "label1";
+            this.C.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // D
+            // 
+            this.D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.D.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.D.Location = new System.Drawing.Point(642, 357);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(372, 113);
+            this.D.TabIndex = 4;
+            this.D.Text = "label1";
+            this.D.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.D.Click += new System.EventHandler(this.D_Click);
+            // 
+            // otazka
+            // 
+            this.otazka.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.otazka.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.otazka.Location = new System.Drawing.Point(86, 51);
+            this.otazka.Name = "otazka";
+            this.otazka.Size = new System.Drawing.Size(1028, 123);
+            this.otazka.TabIndex = 5;
+            this.otazka.Text = "label1";
+            this.otazka.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dalsia_otazka
+            // 
+            this.dalsia_otazka.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dalsia_otazka.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dalsia_otazka.Location = new System.Drawing.Point(631, 14);
+            this.dalsia_otazka.Name = "dalsia_otazka";
+            this.dalsia_otazka.Size = new System.Drawing.Size(199, 40);
+            this.dalsia_otazka.TabIndex = 6;
+            this.dalsia_otazka.Text = "Nahraj otázku";
+            this.dalsia_otazka.UseVisualStyleBackColor = true;
+            this.dalsia_otazka.Click += new System.EventHandler(this.dalsia_otazka_Click);
+            // 
+            // z50
+            // 
+            this.z50.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.z50.Image = ((System.Drawing.Image)(resources.GetObject("z50.Image")));
+            this.z50.Location = new System.Drawing.Point(1449, 9);
+            this.z50.Name = "z50";
+            this.z50.Padding = new System.Windows.Forms.Padding(5);
+            this.z50.Size = new System.Drawing.Size(140, 140);
+            this.z50.TabIndex = 7;
+            this.z50.Click += new System.EventHandler(this.z50_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(200, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(664, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 39);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "B";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(202, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 39);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "C";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(662, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 39);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "D";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // hodnotenie
+            // 
+            this.hodnotenie.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.hodnotenie.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hodnotenie.Location = new System.Drawing.Point(846, 14);
+            this.hodnotenie.Name = "hodnotenie";
+            this.hodnotenie.Size = new System.Drawing.Size(199, 40);
+            this.hodnotenie.TabIndex = 12;
+            this.hodnotenie.Text = "Vyhodnoť odpoveď";
+            this.hodnotenie.UseVisualStyleBackColor = true;
+            this.hodnotenie.Click += new System.EventHandler(this.hodnotenie_Click);
+            // 
+            // zTelefon
+            // 
+            this.zTelefon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zTelefon.Image = ((System.Drawing.Image)(resources.GetObject("zTelefon.Image")));
+            this.zTelefon.Location = new System.Drawing.Point(1605, 9);
+            this.zTelefon.Name = "zTelefon";
+            this.zTelefon.Padding = new System.Windows.Forms.Padding(5);
+            this.zTelefon.Size = new System.Drawing.Size(140, 140);
+            this.zTelefon.TabIndex = 13;
+            this.zTelefon.Click += new System.EventHandler(this.zTelefon_Click);
+            // 
+            // ukoncenie
+            // 
+            this.ukoncenie.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ukoncenie.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ukoncenie.Location = new System.Drawing.Point(410, 14);
+            this.ukoncenie.Name = "ukoncenie";
+            this.ukoncenie.Size = new System.Drawing.Size(199, 40);
+            this.ukoncenie.TabIndex = 15;
+            this.ukoncenie.Text = "Ukončiť hru";
+            this.ukoncenie.UseVisualStyleBackColor = true;
+            this.ukoncenie.Click += new System.EventHandler(this.ukoncenie_Click);
+            // 
+            // graf
+            // 
+            this.graf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            chartArea2.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            chartArea2.Name = "ChartArea1";
+            this.graf.ChartAreas.Add(chartArea2);
+            this.graf.Location = new System.Drawing.Point(608, -188);
+            this.graf.Name = "graf";
+            this.graf.Size = new System.Drawing.Size(377, 257);
+            this.graf.TabIndex = 16;
+            this.graf.Text = "chart1";
+            // 
+            // zPublikum
+            // 
+            this.zPublikum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zPublikum.Image = ((System.Drawing.Image)(resources.GetObject("zPublikum.Image")));
+            this.zPublikum.Location = new System.Drawing.Point(1765, 9);
+            this.zPublikum.Name = "zPublikum";
+            this.zPublikum.Padding = new System.Windows.Forms.Padding(5);
+            this.zPublikum.Size = new System.Drawing.Size(140, 140);
+            this.zPublikum.TabIndex = 17;
+            this.zPublikum.Click += new System.EventHandler(this.zPublikum_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.otazka);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.A);
+            this.panel1.Controls.Add(this.C);
+            this.panel1.Controls.Add(this.B);
+            this.panel1.Controls.Add(this.D);
+            this.panel1.Location = new System.Drawing.Point(137, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 525);
+            this.panel1.TabIndex = 18;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.hodnotenie);
+            this.panel2.Controls.Add(this.dalsia_otazka);
+            this.panel2.Controls.Add(this.ukoncenie);
+            this.panel2.Location = new System.Drawing.Point(12, 712);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1869, 70);
+            this.panel2.TabIndex = 19;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(1644, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 39);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Nová hra";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // priatel
+            // 
+            this.priatel.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.priatel.Location = new System.Drawing.Point(591, 1);
+            this.priatel.Name = "priatel";
+            this.priatel.Size = new System.Drawing.Size(635, 23);
+            this.priatel.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1998, 794);
+            this.Controls.Add(this.graf);
+            this.Controls.Add(this.priatel);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.zPublikum);
+            this.Controls.Add(this.zTelefon);
+            this.Controls.Add(this.z50);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Milionár";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graf)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label A;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label C;
+        private System.Windows.Forms.Label D;
+        private System.Windows.Forms.Label otazka;
+        private System.Windows.Forms.Button dalsia_otazka;
+        private System.Windows.Forms.Label z50;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button hodnotenie;
+        private System.Windows.Forms.Label zTelefon;
+        private System.Windows.Forms.Button ukoncenie;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graf;
+        private System.Windows.Forms.Label zPublikum;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label priatel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
